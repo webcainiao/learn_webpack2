888d7c4b69f07195cdddda97d74e977561f02ad7
webcainiao <2401800893@qq.com>
2017-03-27 17:35:31 +0800

    dddd

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-import _ from 'lodash';
+import _ from 'lodash';//此处引入的是一个第三方库,不会经常变化,这种引入会每次都被打包
+import '../src/index.css';
 function component(){
 	var element = document.createElement('div');
 	element.innerHTML = _.join(['Hello','Webpack','so easy'],' ');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports  = {
-	entry: './app/index.js',
+	entry: {
+		main:'./app/index.js',
+		vendor: 'lodash'//此处lodash为安装的实际包名
+	},
 	output: {
 		path: path.resolve(__dirname,'dist'),
-		filename: 'bundle.js'
-	}
+		filename: '[chunkhash].[name].bundle.js'
+	},
+  plugins:[
+		new webpack.optimize.CommonsChunkPlugin({
+			name:'vendor'//指定公共bundle的名称
+		})
+	]
 }
