523271588269af73297010335a5c50b6548f28f9
webcainiao <2401800893@qq.com>
2017-03-28 23:20:35 +0800

    home

--- config/base.js
+++ config/base.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function(env){
+	//设置共用的功能
 	return {
 		entry: {
 			main: '../app/index.js',
@@ -17,10 +19,30 @@ module.exports = function(env){
 			modules: [path.join(__dirname,'../app'),'../node_modules']
 		},
 		module: {
-			
+			rules: [
+				{
+					test: /\.css/,
+					use: ['style-loader','css-loader']
+				},
+				{
+					test: /\.(png|jpeg|jpg|gif)$/,
+					use: 'file-loader'
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|svg)$/,
+					use: 'url-loader?limit=10000'
+				},
+			]
 		},
 		plugins: [
-
+			new ForkCheckerPlugin(),
+			new webpack.optimize.CommonsChunkPlugin({
+				name: ['main','vendor'].reverse()
+			}),
+			new HtmlWebpackPlugin({
+				template: '../index.html',
+				chunksSortMode: 'dependency'
+			})
 		]
 	}
 
--- config/prod.js
+++ config/prod.js
@@ -1,20 +1,22 @@
 const path =  require('path');
 const webpack= require('webpack');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./base.js');
 
 module.exports =  function(env){
-	return {
-		output: {
-			path: path.resolve(__dirname,'../dist/assets'),
-			filename: '[name].bundle.js',
-			publicPath: publicPath,
-			sourceMapFilename: '[name].map'
-		},
+	return webpackMerge(commonConfig(),{//使用merge合并base
+		// output: {//共用base里面设置，这里省略
+		// 	path: path.resolve(__dirname,'../dist/assets'),
+		// 	filename: '[name].bundle.js',
+		// 	publicPath: publicPath,
+		// 	sourceMapFilename: '[name].map'
+		// },
 		plugins: [
 			new webpack.LoaderOptionsPlugin({
 				minimize: true,
 				debug: false
 			}),
-			new UglifyJsPlugin({
+			new UglifyJsPlugin({//webpack.optimize.UglifyJsPlugin
 				beautify: false,
 				mangle: {
 					screw_ie8: true,
@@ -24,7 +26,12 @@ module.exports =  function(env){
 					screw_ie8: true
 				},
 				comments: false
+			}),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringfy('prod');
+				}
 			})
 		]
-	}
+	})
 }
